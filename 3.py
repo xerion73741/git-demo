352ed487765abd5c62bbfc7d47d464ac7c5ac904
jason <xeriop12426@gmail.com>
2025-07-21 13:03:27 +0800

    增加重複、例外處理

--- 3.py
+++ 3.py
@@ -1,15 +1,27 @@
-# a = eval(input('請輸入第一個數: '))
-# b = eval(input('請輸入第二個數: '))
-# c = abs(a-b)
+# 1. 加入例外處理
+# 2. 重複輸入, 直到a=>exit
 
-# if a>b: 
-#     print(f'a 比較大, 差了 {c}')
+while True:
 
-# elif b>a:
-#     print(f'b 比較大, 差了 {c}')
+    a = input('請輸入第一個數(exit 離開):\n')
+    if a == 'exit':
+        print('停止運作')
+        break
+    try:
+        a = eval(a)
+        b = eval(input('請輸入第二個數:\n'))
+        
+        if a>b: 
+            print(f'a 比較大, 差了 {a-b}\n')
 
-# else: print('一樣大')
+        elif b>a:
+            print(f'b 比較大, 差了 {b-a}\n')
 
-numbers = input("請輸入a,b兩個數字:")
-numbers = list(map(eval,numbers.split(",")))
-print(numbers)
+        else: print('一樣大')
+    except Exception as e:
+        print(f'發生錯誤: {e}')
+
+
+# numbers = input("請輸入a,b兩個數字:")
+# numbers = list(map(eval,numbers.split(",")))
+# print(numbers)
